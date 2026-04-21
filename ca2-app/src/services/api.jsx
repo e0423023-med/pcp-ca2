339931ad18916cb052f7a2649c094592eaad6772
e0423023-med <e0423023@sriher.edu.in>
2026-04-21 08:36:42 +0530

    app completed git commit

---
+++ ca2-app/src/services/api.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const BASE_URL = "https://t4e-testserver.onrender.com/api";
+
+export const getToken = async (studentId, password, set) => {
+  const { data } = await axios.post(`${BASE_URL}/public/token`, {
+    studentId: E0423023,
+    password: 581442,
+    set : "set A",
+  });
+  return data;
+};
+
+export const getDataset = async (token, dataUrl) => {
+  const { data } = await axios.get(`${BASE_URL}${dataUrl}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return data.data;
+};
